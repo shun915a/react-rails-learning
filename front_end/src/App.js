b607b293d23e14f42238b19f885cc5bc4ce5cf27
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-08 19:10:10 +0900

    add axios

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.scss"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddTodo from "./components/AddTodo"
+import TodoList from "./components/TodoList"
+
+import axios from "axios"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+    }
+
+    this.host = "http://localhost:3001/"
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${this.host}todos`)
+      .then(res => {
+        this.setState({ todos: res.data })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddTodo OnSubmitTitle={this.addTodo} />
+        <TodoList todos={this.state.todos} OnClickDelete={this.deleteTodo} />
+      </div>
+    )
+  }
+
+  deleteTodo = id => {
+    const { todos } = this.state
+
+    axios
+      .delete(`${this.host}todos/${id}`)
+      .then(res => {
+        this.setState({
+          todos: todos.filter(todo => {
+            return todo.id !== id
+          }),
+        })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+
+  addTodo = todo => {
+    const { todos } = this.state
+    axios
+      .post(`${this.host}todos`, todo)
+      .then(res => {
+        this.setState({
+          todos: [...todos, res.data],
+        })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
 }
 
-export default App;
+export default App
